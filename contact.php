c20b08ff9677f131b75f0a227f1c33f1aa55fefa
Sahil Dhull <sahildhull25121998@gmail.com>
2019-12-09 20:13:22 +0530

    update

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+echo $_POST["name"];
+// if($_POST["submit"]) {
+// 	echo "ok"
+//     // mail("sdhull@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+// }
+?>
