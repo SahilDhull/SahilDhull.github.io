db9c434653b875397c2e8474c9a5829155fc5311
Sahil Dhull <sdhull2512@gmail.com>
2026-02-17 23:43:40 +0530

    redirect to sahildhull.com

--- contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo $_POST["name"];
-// if($_POST["submit"]) {
-// 	echo "ok"
-//     // mail("sdhull@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
-// }
-?>
